7007bf439155089a9075b21a58631e3488e4b719
mearnin <87330597+mearnin@users.noreply.github.com>
2021-08-26 10:52:01 +0530

    Update asuna.py

--- wbb/modules/asuna.py
+++ wbb/modules/asuna.py
@@ -15,12 +15,12 @@ REPLY = (
 "Don't get fun with my name!!! I don't like it🤬",
 "Don't forget to recommend me to your friends❤️😎")
 
-@app.on_message(filters.text("m"))
+@app.on_message(filters.text('m', "Asuna"))
 async def Asuna(_, message):
     if message.chat.type != "private":
       await message.reply_text(random.choice(REPLY))
 
-@app.on_message(filters.text("m"))
+@app.on_message(filters.text('m', "asuna"))
 async def asuna(_, message):
     if message.chat.type != "private":
       await message.reply_text(random.choice(REPLY))
