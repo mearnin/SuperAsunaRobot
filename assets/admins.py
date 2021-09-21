b7e3d4c57208a6cd40a6e8dbe9a8413551141a80
mearnin <87330597+mearnin@users.noreply.github.com>
2021-09-21 09:04:05 +0530

    Update admins.py

--- assets/admins.py
+++ assets/admins.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
-from config import admins
+from sample_config import admins
 from typing import Dict, List
 
 admins: Dict[int, List[int]] = {}
